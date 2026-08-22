b7f21bf323b0366c7a046b7592f2ffa3d08c67b6
hansferdinand <125700248+hansferdinand@users.noreply.github.com>
2026-08-22 17:15:17 -0500

    feat: add device invite dock to GM console

--- app/control/[room]/page.tsx
+++ app/control/[room]/page.tsx
@@ -1,6 +1,12 @@
 import { ControlPanel } from "@/components/ControlPanel";
+import { DeviceInvitePanel } from "@/components/DeviceInvitePanel";
 
 export default async function ControlPage({ params }: { params: Promise<{ room: string }> }) {
   const { room } = await params;
-  return <ControlPanel room={room} />;
+  return (
+    <>
+      <ControlPanel room={room} />
+      <DeviceInvitePanel room={room} />
+    </>
+  );
 }
